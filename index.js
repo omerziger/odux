62fb83616bf32465ffbfc6268b48c6686933af9c
greenbackboogie97 <greenbackboogie@tutamail.com>
2023-04-29 16:35:01 +0300

    Simple working redux

---
+++ index.js
@@ -0,0 +1,32 @@
+const cartInitialState = {
+    items: [],
+    promotions: [],
+    cost: 0
+};
+
+class Store {
+    constructor(initialState, reducer) {
+        this.state = initialState;
+        this.reducer = reducer;
+    }
+
+    dispatch(action, payload) {
+        this.state = this.reducer(this.state, action, payload);
+    }
+}
+
+const reducer = (state, action, payload) => {
+    switch (action) {
+        case 'SET_ITEMS':
+            state.items = payload;
+            break;
+        default:
+            break;
+    }
+
+    return state;
+}
+
+const cartStore = new Store(cartInitialState, reducer);
+
+cartStore.dispatch('SET_ITEMS', ['1', '2', '3']);
